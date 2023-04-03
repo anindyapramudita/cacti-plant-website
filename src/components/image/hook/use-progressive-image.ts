b14f324459914ad95ff13f100df193792b2f0f26
Anindya Pramudita <anindyapramudita2111@gmail.com>
2023-04-03 21:36:50 +0700

    feat(image): create custom hook for image loader

---
+++ src/components/image/hook/use-progressive-image.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+interface IProgressiveImageProps {
+  lowQualityImage: string;
+  highQualityImage: string;
+}
+
+export const useProgressiveImage = ({lowQualityImage, highQualityImage}:IProgressiveImageProps) => {
+  const [src, setSrc] = useState(lowQualityImage)
+
+  useEffect(() => {
+    setSrc(lowQualityImage);
+    
+    const img = new Image();
+    img.src = highQualityImage;
+
+    img.onload = () => {
+      setSrc(highQualityImage)
+    }
+  }, [lowQualityImage, highQualityImage])
+  
+return [{src, blur: src !== highQualityImage}];
+}
