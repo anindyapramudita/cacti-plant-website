ec6137900cb0adc3afa0366d7b541f86cf8c9d6f
Anindya Pramudita <anindyapramudita2111@gmail.com>
2023-04-03 21:30:30 +0700

    feat(setup): initial commit for project setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  images: {
+    domains: ['images.unsplash.com']
+  },
+  domains: ['localhost:3000']
 }
 
 module.exports = nextConfig
