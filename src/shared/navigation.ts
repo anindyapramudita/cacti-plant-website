79d274b977039354ebe21b6d3714351dc167eda3
Anindya Pramudita <anindyapramudita2111@gmail.com>
2023-04-05 00:17:36 +0700

    feat(shared): create navigation path

---
+++ src/shared/navigation.ts
@@ -0,0 +1,27 @@
+export const navigation = [
+  {
+    path:'/',
+    name: 'Discovery',
+    roles: ['admin', 'user', 'all']
+  },
+  {
+    path:'/search',
+    name: 'Search',
+    roles: ['admin', 'user', 'all']
+  },
+  {
+    path:'/login',
+    name: 'Login',
+    roles: ['admin', 'user', 'all']
+  },
+  {
+    path:'/register',
+    name: 'Register',
+    roles: ['admin', 'user', 'all']
+  },
+  {
+    path:'/profile',
+    name: 'Profile',
+    roles: ['user']
+  },
+]
