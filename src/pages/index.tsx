cccc1f0e6be03d3eb03909841da96ad5fc0da9f2
Anindya Pramudita <anindyapramudita2111@gmail.com>
2023-04-05 00:20:53 +0700

    chore(index): refactor homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,59 @@
-import { TransitionImage } from "@/components/image";
 import config from "@/shared/config";
 import { plantDataType } from "@/shared/types";
 import { CardLayout } from "@/components/card-layout";
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import { HomeLayout } from "@/components/home-layout";
 import { Header } from "@/components/header";
+import { Card } from "@/components/card";
+import { useMemo } from "react";
+import useDeviceSize from "@/hooks/use-device-size";
 
 type plantData = {
-  plants: plantDataType[]
-}
+  plants: plantDataType[];
+};
 
-export default function Home({
-plants
-}: plantData) {
+export default function Home({ plants }: plantData) {
+  const [width] = useDeviceSize();
 
+  const cardData = useMemo(() => {
+    let newData = [...plants];
+    if (width < 576 && width > 0) {
+      newData = [newData[0]];
+      return newData;
+    } else if (width < 992) {
+      newData = [newData[0], newData[1]];
+      return newData;
+    } else {
+      return newData;
+    }
+  }, [plants, width]);
+  
   return (
-    // <HomeLayout>
-    <>
-      <Header/>
+    <HomeLayout>
+      <Header />
       <CardLayout>
-        {
-          plants.map((plant) => (
-            <TransitionImage
-              placeholder={plant.images.placeholder}
-              image={plant.images.mainImage}
-              alt={plant.images.alt}
-            />
-          ))
-          }
+        {cardData.map((plant) => (
+          <Card data={plant} />
+        ))}
       </CardLayout>
-    </>
-    // </HomeLayout>
+    </HomeLayout>
   );
 }
 
-export async function getServerSideProps({req, res}: {req: NextApiRequest, res: NextApiResponse<plantDataType>}) {
-
-  const fetchAPI = await fetch(`${config.databaseUrl}/plants`)
-  const data: plantDataType = await fetchAPI.json()
+export async function getServerSideProps({
+  req,
+  res,
+}: {
+  req: NextApiRequest;
+  res: NextApiResponse<plantDataType>;
+}) {
+  const fetchAPI = await fetch(`${config.databaseUrl}/plants`);
+  const data: plantDataType[] = await fetchAPI.json();
   const error = fetchAPI.ok ? false : fetchAPI.statusText;
 
   return {
     props: {
-      plants: data
-  }
-  }
+      plants: data,
+    },
+  };
 }
-
