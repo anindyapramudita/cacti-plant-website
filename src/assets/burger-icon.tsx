7d96cd3c0813e18f4a68b5620c686b56a185958a
Anindya Pramudita <anindyapramudita2111@gmail.com>
2023-04-04 18:05:32 +0700

    feat(assets): add new assets for logo and icon

---
+++ src/assets/burger-icon.tsx
@@ -0,0 +1,42 @@
+import { FC, SVGAttributes } from "react";
+
+interface IIconProps {
+  color: string;
+  width: number;
+  height: number;
+}
+
+export const BurgerIcon: FC<SVGAttributes<IIconProps>> = ({
+  color = "#000000",
+  width = 30,
+  height = 30,
+}) => {
+  return (
+    <svg
+      width={`${width}px`}
+      height={`${height}px`}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 18L20 18"
+        stroke={color}
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+      <path
+        d="M4 12L20 12"
+        stroke={color}
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+      <path
+        d="M4 6L20 6"
+        stroke={color}
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
