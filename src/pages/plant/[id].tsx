142fd8c72ed3a7212334820db0bd9550fe7dedd1
Anindya Pramudita <anindyapramudita2111@gmail.com>
2023-06-18 16:23:07 +0700

    feat(plant details page): add functionality

--- src/pages/plant/[id].tsx
+++ src/pages/plant/[id].tsx
@@ -2,35 +2,64 @@ import config from "@/shared/config";
 import { plantDataType } from "@/shared/types";
 import { HomeLayout } from "@/components/layouts/home";
 import { Header } from "@/components/header";
-import { TransitionImage } from "@/components/image";
 import { PlantDetailsLayout } from "@/components/layouts/plant-details";
 import { OverviewCard } from "@/components/overview-card";
 import { ShuffleButton } from "@/components/shuffle-button";
 import { FavoriteButton } from "@/components/favorite-button";
+import { useState } from "react";
+import { CarouselImage } from "@/components/carousel-image";
+import randomId from "@/shared/utils/generateRandomId";
+import { useRouter } from "next/navigation";
 
 type plantData = {
   plants: plantDataType;
+  id: number;
 };
 
-export default function PlantDetails({ plants }: plantData) {
+export default function PlantDetails({ plants, id }: plantData) {
+  const [currentId, setCurrentId] = useState<number>(id);
+  const [currentData, setCurrentData] = useState<plantDataType>(plants);
+  const [restartImage, setRestartImage] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const handleShuffleData = async () => {
+    try {
+      const newId = randomId(currentId);
+      setCurrentId(newId);
+      setRestartImage(true);
+
+      const fetchAPI = await fetch(`${config.databaseUrl}/plants/${newId}`);
+      const response = await fetchAPI.json();
+      if (response) {
+        router.push(`/plant/${newId}`);
+        setCurrentData(response);
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <HomeLayout>
       <Header />
       <PlantDetailsLayout>
         <div className="plant-image">
-          <TransitionImage
-            image={plants.images.mainImage}
-            placeholder={plants.images.placeholder}
-            alt={plants.images.alt}
+          <CarouselImage
+            images={currentData?.images}
+            restartImage={restartImage}
+            setRestartImage={setRestartImage}
           />
           <FavoriteButton />
         </div>
         <div className="plant-details">
-          <h3>{plants.name.toUpperCase()}</h3>
-          <OverviewCard data={plants} />
+          <h3>{currentData?.name.toUpperCase()}</h3>
+          <OverviewCard data={currentData} />
         </div>
       </PlantDetailsLayout>
-      <ShuffleButton />
+      <ShuffleButton handleShuffle={handleShuffleData} />
     </HomeLayout>
   );
 }
@@ -44,6 +73,7 @@ export async function getServerSideProps(context: any) {
   return {
     props: {
       plants: data,
+      id,
     },
   };
 }
