460a32b17a0bca298867391d6d0e251d0bf15822
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-03 22:34:35 +0700

    feat(carousel-image): refactor code

--- src/components/carousel-image/carousel-image.interface.ts
+++ src/components/carousel-image/carousel-image.interface.ts
@@ -1,19 +1,8 @@
 import { plantImagesType } from "@/shared/types";
-import { Dispatch, HTMLAttributes, SetStateAction } from "react";
+import { Dispatch, SetStateAction } from "react";
 
 export interface ICarouselImage {
   images: plantImagesType[];
   restartImage: boolean;
   setRestartImage: Dispatch<SetStateAction<boolean>>;
 }
-
-// export interface IImageProps extends HTMLAttributes<HTMLImageElement> {
-//   image: string;
-//   placeholder: string;
-//   alt: string;
-// }
-
-// export interface IImageProps extends HTMLAttributes<HTMLImageElement>{
-//   lowQualityImage: string;
-//   highQualityImage: any;
-// }
