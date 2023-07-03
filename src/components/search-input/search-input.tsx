da050ac9b34062357c1a1f68535aabaea55c75c5
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-03 22:46:59 +0700

    feat(search-input): integrate reducer to filter

--- src/components/search-input/search-input.tsx
+++ src/components/search-input/search-input.tsx
@@ -1,14 +1,26 @@
-import { SearchIcon } from "@/assets/search-icon";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ISeachInputProps } from "./search-input.interface";
 import { StylesWrapper } from "./search-input.styles";
+import { usePlantFilter } from "@/hooks/use-plant-filter";
+import { AiOutlineSearch } from "react-icons/ai";
 
-export const SearchInput: FC<ISeachInputProps> = ({}) => {
+export const SearchInput: FC<ISeachInputProps> = () => {
+  const { dispatch } = usePlantFilter();
+
+  const [searchText, setSearchText] = useState<string>("");
+  const handleSearch = () => {
+    dispatch({ type: "search.plant", payload: searchText });
+  };
+
   return (
     <StylesWrapper>
-      <input type="textbox" placeholder="search..." />
-      <button className="search-button">
-        <SearchIcon width={15} />
+      <input
+        type="textbox"
+        placeholder="search..."
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button className="search-button" onClick={handleSearch}>
+        <AiOutlineSearch size={15} />
       </button>
     </StylesWrapper>
   );
