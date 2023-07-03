53eb3ff06d2427b9f7f4dae443e7bb2daadb8f35
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-03 22:33:33 +0700

    feat(card): refactor code

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,5 +1,3 @@
-import { ChevronDownIcon } from "@/assets/chevron-down-icon";
-import { ChevronUpIcon } from "@/assets/chevron-up-icon";
 import config from "@/shared/config";
 import Link from "next/link";
 import { FC, useState } from "react";
@@ -8,6 +6,7 @@ import { FavoriteButton } from "../favorite-button";
 import { TransitionImage } from "../image";
 import { ICardProps } from "./card.interface";
 import { StylesWrapper } from "./card.styles";
+import { BiChevronUp, BiChevronDown } from "react-icons/bi";
 
 export const Card: FC<ICardProps> = ({ data }) => {
   const [showSummary, setShowSummary] = useState<boolean>(false);
@@ -18,23 +17,16 @@ export const Card: FC<ICardProps> = ({ data }) => {
 
   return (
     <StylesWrapper showSummary={showSummary}>
-      {showSummary ? (
-        <div
-          className="swipe-up-icon"
-          onClick={() => setShowSummary(!showSummary)}
-        >
-          <ChevronDownIcon color="white" />
-          <p>hide summary</p>
-        </div>
-      ) : (
-        <div
-          className="swipe-up-icon"
-          onClick={() => setShowSummary(!showSummary)}
-        >
-          <ChevronUpIcon color="white" />
-          <p>see summary</p>
-        </div>
-      )}
+      <div
+        role="button"
+        tabIndex={0}
+        className="swipe-up-icon"
+        onClick={() => setShowSummary(!showSummary)}
+        onKeyDown={() => setShowSummary(!showSummary)}
+      >
+        {showSummary ? <BiChevronDown size={40} /> : <BiChevronUp size={40} />}
+        <p>{showSummary ? "hide summary" : "see summary"}</p>
+      </div>
       <div className="card-overlay">
         <CardOverlay data={data} />
       </div>
