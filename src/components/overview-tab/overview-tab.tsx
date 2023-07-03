aa55db41d608e26c6376f8f62be85fc4a998efbd
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-03 22:42:15 +0700

    feat(overview-tab): refactor code using utils

--- src/components/overview-tab/overview-tab.tsx
+++ src/components/overview-tab/overview-tab.tsx
@@ -1,49 +1,12 @@
-import { InformationIcon } from "@/assets/information-icon";
-import { PlantIcon } from "@/assets/plant-icon";
-import { SignalIcon } from "@/assets/signal-icon";
-import { SummerIcon } from "@/assets/summer-icon";
-import { WaterIcon } from "@/assets/water-icon";
 import { FC } from "react";
 import { IOverviewTabProps } from "./overview-tab.interface";
 import { StylesWrapper, TabWrapper } from "./overview-tab.styles";
-
-const tabInfo = [
-  {
-    name: "Information",
-    activeIcon: <InformationIcon color="#123132" />,
-    nonActiveIcon: <InformationIcon color="white" />,
-  },
-  {
-    name: "Water Content",
-    activeIcon: <WaterIcon color="#123132" />,
-    nonActiveIcon: <WaterIcon color="white" />,
-  },
-  {
-    name: "Season",
-    activeIcon: <SummerIcon color="#123132" />,
-    nonActiveIcon: <SummerIcon color="white" />,
-  },
-  {
-    name: "Plant Size",
-    activeIcon: <PlantIcon color="#123132" />,
-    nonActiveIcon: <PlantIcon color="white" />,
-  },
-  {
-    name: "Difficulty",
-    activeIcon: <SignalIcon color="#123132" />,
-    nonActiveIcon: <SignalIcon color="white" />,
-  },
-];
+import { tabInfo } from "./utils/overview-tab-info";
 
 export const OverviewTab: FC<IOverviewTabProps> = ({
   onClickTab,
   activeTab,
 }) => {
-  // const [activeTab, setActiveTab] = useState<number>(0);
-  // if (!children) {
-  //   return null;
-  // }
-
   return (
     <StylesWrapper data-testid="overview-tab">
       {tabInfo.map((tab, index) =>
