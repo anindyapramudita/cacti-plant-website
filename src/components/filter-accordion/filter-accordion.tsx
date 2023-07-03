bc72b280eddb9f5f59e8c1e2b26fa2a5c522cc49
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-03 22:39:21 +0700

    feat(filter-accordion): refactor using utils

--- src/components/filter-accordion/filter-accordion.tsx
+++ src/components/filter-accordion/filter-accordion.tsx
@@ -1,37 +1,13 @@
-import { LeafIcon } from "@/assets/leaf-icon";
-import { LevelHighIcon } from "@/assets/level-high-icon";
-import { LevelLowIcon } from "@/assets/level-low-icon";
-import { LevelMedIcon } from "@/assets/level-med-icon";
-import { SnowIcon } from "@/assets/snow-icon";
-import { SpringIcon } from "@/assets/spring-icon";
-import { SummerIcon } from "@/assets/summer-icon";
-import { WaterIcon } from "@/assets/water-icon";
 import { FC } from "react";
 import { Button } from "../button";
 import { ButtonGroup } from "../button-group";
 import { IFilterAccordionProps } from "./filter-accordion.interface";
 import { StylesWrapper } from "./filter-accordion.styles";
-
-const waterFilter = [
-  { label: "low", value: 1, icon: <WaterIcon width={10} /> },
-  { label: "med", value: 2, icon: <WaterIcon width={15} /> },
-  { label: "high", value: 3, icon: <WaterIcon width={17} /> },
-];
-
-const seasonFilter = [
-  { label: "spring", value: "spring", icon: <SpringIcon width={18} /> },
-  { label: "summer", value: "summer", icon: <SummerIcon width={20} /> },
-  { label: "autumn", value: "autumn", icon: <LeafIcon width={15} /> },
-  { label: "winter", value: "winter", icon: <SnowIcon width={18} /> },
-];
-
-const difficultyFilter = [
-  { label: "easy", value: 1, icon: <LevelLowIcon width={20} /> },
-  { label: "medium", value: 2, icon: <LevelMedIcon width={20} /> },
-  { label: "hard", value: 3, icon: <LevelHighIcon width={20} /> },
-];
-
-type FilterCategory = "water" | "season" | "difficulty";
+import {
+  difficultyFilter,
+  seasonFilter,
+  waterFilter,
+} from "./utils/season-filter";
 
 export const FilterAccordion: FC<IFilterAccordionProps> = ({ isOpen }) => {
   return (
@@ -55,7 +31,7 @@ export const FilterAccordion: FC<IFilterAccordionProps> = ({ isOpen }) => {
             />
           </div>
           <div>
-            <p>Difficulty Level:</p>
+            <p>Care Level:</p>
             <ButtonGroup
               buttons={difficultyFilter}
               category="difficulty"
