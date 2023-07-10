71bba99f9637e5ed5eb119f321bc3dd7350e03cb
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-10 17:50:37 +0700

    feat(overview-card): update with new utils

--- src/components/overview-card/overview-card.tsx
+++ src/components/overview-card/overview-card.tsx
@@ -2,18 +2,16 @@ import { FC, useMemo, useState } from "react";
 import { OverviewTab } from "../overview-tab";
 import { IOverviewCardProps } from "./overview-card.interface";
 import { StylesWrapper, OverviewCardWrapper } from "./overview-card.styles";
-
-const tabInfo = ["description", "water", "season", "size", "difficulty"];
+import { seasonList, tabInfo } from "./utils/overview-card-info";
 
 export const OverviewCard: FC<IOverviewCardProps> = ({ data }) => {
   const [activeTab, setActiveTab] = useState<number>(0);
 
   const renderText = useMemo(() => {
-    const seasonList = ["Spring", "Summer", "Fall", "Winter"];
     const activeSeason: string[] = [];
 
     seasonList.forEach((season) => {
-      if (data.season[season.toLowerCase()] === true) {
+      if (data.seasons[season.toLowerCase()] === true) {
         activeSeason.push(season);
       }
     });
---
+++ src/components/overview-card/utils/overview-card-info.ts
@@ -0,0 +1,2 @@
+export const tabInfo = ["description", "water", "season", "size", "care"];
+export const seasonList = ["Spring", "Summer", "Fall", "Winter"];
