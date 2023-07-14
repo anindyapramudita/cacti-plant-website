8612176fecca692f88e3e4e33cb307d3c1eb6ade
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-14 20:38:37 +0700

    refactor(_app): add layout and header to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
+import { Header } from "@/components/header";
+import { HomeLayout } from "@/components/layouts/home";
+import { LoginModal } from "@/components/login-modal";
 import { GlobalStyles } from "@/styles/globals";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <HomeLayout>
+        <Header onLogin={() => setLoginOpen(!loginOpen)} />
+        <Component {...pageProps} />
+        <LoginModal open={loginOpen} onClose={() => setLoginOpen(false)} />
+      </HomeLayout>
     </>
   );
 }
