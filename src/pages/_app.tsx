718484e0c0ba2239d656b69ebe250d20d7822103
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-18 13:11:24 +0200

    feat(_app): link next-auth and create loading page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,53 @@ import { HomeLayout } from "@/components/layouts/home";
 import { LoginModal } from "@/components/login-modal";
 import { GlobalStyles } from "@/styles/globals";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SessionProvider } from "next-auth/react";
+import { useRouter } from "next/router";
+import { Loading } from "@/components/loading-page/";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setLoading(true);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router.events]);
+
+  const openLoginModal = () => setLoginOpen(true);
 
   return (
     <>
       <GlobalStyles />
-      <HomeLayout>
-        <Header onLogin={() => setLoginOpen(!loginOpen)} />
-        <Component {...pageProps} />
-        <LoginModal open={loginOpen} onClose={() => setLoginOpen(false)} />
-      </HomeLayout>
+      <SessionProvider session={session}>
+        {loading ? (
+          <Loading />
+        ) : (
+          <HomeLayout>
+            <Header onLogin={() => setLoginOpen(!loginOpen)} />
+            <Component {...pageProps} openLoginModal={openLoginModal} />
+            <LoginModal open={loginOpen} onClose={() => setLoginOpen(false)} />
+          </HomeLayout>
+        )}
+      </SessionProvider>
     </>
   );
 }
