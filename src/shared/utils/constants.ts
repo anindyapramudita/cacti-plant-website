76765b8ed0f4c2586327b1291f2525401470affb
anindyapramudita <anindyapramudita2111@gmail.com>
2023-07-19 13:20:24 +0200

    feat(constants): expose new constants file

---
+++ src/shared/utils/constants.ts
@@ -0,0 +1,33 @@
+export const SIGN_OUT = "Sign Out";
+export const SIGN_IN = "Sign In";
+export const SIGN_UP = "Sign Up";
+
+export const SAVE = "Save";
+export const CLEAR = "Clear";
+
+export const HEADER_USER_NAME = "Welcome back,";
+export const LOGIN_HEADER = "Welcome back!";
+export const REGISTER_HEADER = "Welcome to";
+export const CACTI = "Cacti";
+
+export const NAME = "Name";
+export const EMAIL = "Email";
+export const PASSWORD = "Password";
+export const PASSWORD_CONFIRMATION = "Password Confirmation";
+
+export const OR = "or";
+
+export const GOOGLE_SIGN_IN = "Sign in with Google";
+
+export const LOADING = "Loading...";
+
+export const SEARCH = "search...";
+
+export const WATER_NEEDS = "Water Needs:";
+export const BLOOM_SEASON = "Bloom Season:";
+export const CARE_LEVEL = "Care Level:";
+
+export const SOMETHING_WRONG = "something is wrong. Please try again";
+export const FORM_ERROR =
+  "Please fill in the forms correctly and have min 6 characters in your password";
+export const PASSWORD_NOT_MATCH = "passwords do not match";
