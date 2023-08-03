e77e3d42db09cf7bf931a050ca95ae278eb392cc
anindyapramudita <anindyapramudita2111@gmail.com>
2023-08-03 12:01:08 +0200

    feat(plant-card-stories): expose story for plant card

--- src/stories/plant-card.stories.tsx
+++ src/stories/plant-card.stories.tsx
@@ -2,19 +2,20 @@ import { PlantCard } from "@/components/refactor-card";
 import { plantCardMock } from "@/components/refactor-card/__tests__/plant-card.mock";
 import type { Meta, StoryObj } from "@storybook/react";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: "Components/PlantCard",
   component: PlantCard,
   parameters: plantCardMock,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ["autodocs"],
+  decorators: [(Story) => (
+    <div style={{ width: '30rem'}}>
+      <Story />
+    </div>
+  )]
 } satisfies Meta<typeof PlantCard>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
   args: plantCardMock,
 };
