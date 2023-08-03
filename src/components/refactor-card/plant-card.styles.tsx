510848e711c49345363019b30dc0875a4bc39590
anindyapramudita <anindyapramudita2111@gmail.com>
2023-08-03 12:09:09 +0200

    style(plant-card): refactor style using css var

--- src/components/refactor-card/plant-card.styles.tsx
+++ src/components/refactor-card/plant-card.styles.tsx
@@ -4,60 +4,72 @@ import styled from "styled-components";
 export const StylesWrapper = styled.div<{
   showSummary: boolean;
 }>`
-  position: relative;
-  overflow: hidden;
+  --card-position: relative;
+  --card-overflow: hidden;
+
+  --icon-position: absolute;
+  --icon-bottom: ${(props) => (props.showSummary ? "20%" : "0")};
+  --icon-left: 50%;
+  --icon-transform: translate(-50%, -50%);
+  --icon-display: flex;
+  --icon-flex-direction: column;
+  --icon-align-items: center;
+  --icon-color: var(--white);
+  --icon-text-shadow: 0.125rem 0.125rem 0.375rem #000000;
+  --icon-transition: bottom 0.5s ease-out;
+  --icon-background-color: transparent;
+  --icon-border: none;
+
+  --overlay-visibility: ${(props) => (!props.showSummary ? "hidden" : "visible")};
+  --overlay-top: ${(props) => (props.showSummary ? "80%" : "100%")};
+  --overlay-background: rgba(18, 49, 50, 0.8);
+  --overlay-width: 100%;
+  --overlay-height: 20%;
+  --overlay-display: flex;
+  --overlay-justify-content: center;
+  --overlay-transition: top 0.5s ease-out, visibility 0.5s ease-out;
+
+  position: var(--card-position);
+  overflow: var(--card-overflow);
 
   .swipe-up-icon {
-    position: absolute;
-    bottom: ${(props) => (props.showSummary ? "20%" : "0")};
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: var(--white);
-    text-shadow: 0.125rem 0.125rem 0.375rem #000000;
-    transition: bottom 0.5s ease-out;
-
-    background-color: transparent;
-    border: none;
+    position: var(--icon-position);
+    bottom: var(--icon-bottom);
+    left: var(--icon-left);
+    transform: var(--icon-transform);
+    display: var(--icon-display);
+    flex-direction: var(--icon-flex-direction);
+    align-items: var(--icon-align-items);
+    color: var(--icon-color);
+    text-shadow: var(--icon-text-shadow);
+    transition: var(--icon-transition);
+    background-color: var(--icon-background-color);
+    border: var(--icon-border);
   }
 
   .card-overlay {
-    display: block;
-    position: absolute;
-    top: ${(props) => (props.showSummary ? "80%" : "100%")};
-    background: rgba(18, 49, 50, 0.8);
-    width: 100%;
-    height: 20%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: var(--white);
-    transition: top 0.5s ease-out;
-  }
+    visibility: var(--overlay-visibility);
+    position: var(--icon-position);
+    top: var(--overlay-top);
+    background: var(--overlay-background);
+    width: var(--overlay-width);
+    height: var(--overlay-height);
+    display: var(--overlay-display);
+    align-items: var(--icon-align-items);
+    justify-content: var(--overlay-justify-content);
+    color: var(--icon-color);
+    transition: var(--overlay-transition);
 
-  @media (min-width: ${gridBreakpoint.sm}) {
-  }
-
-  @media (min-width: ${gridBreakpoint.md}) {
   }
 
   @media (min-width: ${gridBreakpoint.lg}) {
-    top: 0%;
-
-    &:hover div {
-      top: 80%;
+    &:hover .card-overlay {
+      --overlay-top: 80%;
+      --overlay-visibility: visible;
     }
 
     .swipe-up-icon {
-      display: none;
+      --icon-display: none;
     }
-  }
-
-  @media (min-width: ${gridBreakpoint.xl}) {
-  }
-
-  @media (min-width: ${gridBreakpoint.xxl}) {
   }
 `;
