1c552af315d17e8a64bacf38b5f6ec18ab0b36f1
anindyapramudita <anindyapramudita2111@gmail.com>
2023-08-03 12:07:13 +0200

    feat(card-overlay-mock): expose mock data for card overlay

---
+++ src/components/refactor-card/components/card-overlay/__tests__/card-overlay.mock.ts
@@ -0,0 +1,6 @@
+import { plantMock } from "../../../../../../__mocks__";
+import { ICardOverlayProps } from "../card-overlay.interface";
+
+export const cardOverlayMock: ICardOverlayProps = {
+  data: plantMock
+};
