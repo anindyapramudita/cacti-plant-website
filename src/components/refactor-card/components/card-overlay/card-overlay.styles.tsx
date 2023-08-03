592cbce8ab9836c29f095da611e6ed48508dce0d
anindyapramudita <anindyapramudita2111@gmail.com>
2023-08-03 12:08:13 +0200

    style(card-overlay): refactor style using css var

--- src/components/refactor-card/components/card-overlay/card-overlay.styles.tsx
+++ src/components/refactor-card/components/card-overlay/card-overlay.styles.tsx
@@ -1,60 +1,60 @@
 import { gridBreakpoint } from "@/shared/breakpoints";
 import styled from "styled-components";
 
-export const StylesWrapper = styled.div<{}>`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 75%;
-  justify-content: space-around;
-  text-align: center;
+export const StylesWrapper = styled.div`
+  --overlay-display: flex;
+  --overlay-direction: column;
+  --overlay-height: 100%;
+  --overlay-width: 75%;
+  --overlay-justify-content: space-around;
+  --overlay-align-items: center;
+  --overlay-text-align: center;
 
-  div {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  --text-flex: 2;
+  --text-align: start;
+
+  display: var(--overlay-display);
+  flex-direction: var(--overlay-direction);
+  height: var(--overlay-height);
+  width: var(--overlay-width);
+  justify-content: var(--overlay-justify-content);
+  text-align: var(--overlay-text-align);
+
+  .category-level {
+    display: var(--overlay-display);
+    align-items: var(--overlay-align-items);
   }
 
-  div p {
-    flex: 2;
-    text-align: start;
-  }
-  @media (min-width: ${gridBreakpoint.sm}) {
+  .level-title {
+    flex: var(--text-flex);
+    text-align: var(--text-align);
   }
 
   @media (min-width: ${gridBreakpoint.md}) {
-    width: 70%;
+    --overlay-width: 70%;
   }
 
   @media (min-width: ${gridBreakpoint.lg}) {
-    width: 65%;
-  }
-
-  @media (min-width: ${gridBreakpoint.xl}) {
+    --overlay-width: 65%;
   }
 
   @media (min-width: ${gridBreakpoint.xxl}) {
-    width: 50%;
+    --overlay-width: 50%;
   }
 `;
 
 export const LevelWrapper = styled.div<{}>`
-  display: flex;
-  justify-content: center;
-  flex: 2;
+  --icon-color: var(--primary-dark);
+  --icon-display: flex;
+  --icon-flex: 2;
+  --icon-justify-content: space-between;
 
-  @media (min-width: ${gridBreakpoint.sm}) {
-  }
+  display: var(--icon-display);
+  justify-content: var(--icon-justify-content);
+  flex: var(--icon-flex);
+  color: var(--icon-color);
 
-  @media (min-width: ${gridBreakpoint.md}) {
-  }
-
-  @media (min-width: ${gridBreakpoint.lg}) {
-  }
-
-  @media (min-width: ${gridBreakpoint.xl}) {
-  }
-
-  @media (min-width: ${gridBreakpoint.xxl}) {
+  .active {
+    color: var(--white);
   }
 `;
