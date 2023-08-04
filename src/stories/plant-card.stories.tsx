f6fb44481934a8ccff3a4c7930ee9ed6d5de01a9
anindyapramudita <anindyapramudita2111@gmail.com>
2023-08-04 13:45:03 +0200

    chore(plant-card-stories): update component route

--- src/stories/plant-card.stories.tsx
+++ src/stories/plant-card.stories.tsx
@@ -1,16 +1,18 @@
-import { PlantCard } from "@/components/refactor-card";
-import { plantCardMock } from "@/components/refactor-card/__tests__/plant-card.mock";
+import { PlantCard } from "@/components/plant-card";
+import { plantCardMock } from "@/components/plant-card/__tests__/plant-card.mock";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
   title: "Components/PlantCard",
   component: PlantCard,
   parameters: plantCardMock,
-  decorators: [(Story) => (
-    <div style={{ width: '30rem'}}>
-      <Story />
-    </div>
-  )]
+  decorators: [
+    (Story) => (
+      <div style={{ width: "30rem" }}>
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof PlantCard>;
 
 export default meta;
