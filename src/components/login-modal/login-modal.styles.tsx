60338c60e61d632d496d480adc99eb8a766f6e8d
anindyapramudita <anindyapramudita2111@gmail.com>
2023-08-10 12:14:13 +0200

    style(login-modal): update style using css variable

--- src/components/login-modal/login-modal.styles.tsx
+++ src/components/login-modal/login-modal.styles.tsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
 export const StylesWrapper = styled.div`
-  width: 20rem;
-  height: 27rem;
+  --modal-width: 20rem;
+  --modal-height: 27rem;
+  --modal-padding: 3rem;
+  --modal-position: relative;
+  --modal-overflow: hidden;
 
-  padding: 3rem;
-
-  position: relative;
-  overflow: hidden;
+  width: var(--modal-width);
+  height: var(--modal-height);
+  padding: var(--modal-padding);
+  position: var(--modal-position);
+  overflow: var(--modal-overflow);
 `;
