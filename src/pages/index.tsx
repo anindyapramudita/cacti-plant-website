f54ebfec11a565ded1ac130e6961750da58b2a11
anindyapramudita <anindyapramudita2111@gmail.com>
2023-08-22 12:26:31 +0200

    chore(homepage): edit image into carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,94 @@
 import { getPlants } from "@/sanity/get-plants";
 import { StylesWrapper } from "./index.styles";
-import Image from "next/image";
+// import Image from "next/image";
 import { PlantDataType } from "@/shared/type/data-types";
-import { imagePlaceholder } from "@/shared/utils/image-placeholder";
+// import { imagePlaceholder } from "@/shared/utils/image-placeholder";
+import { ImageCard } from "@/components/image-card";
+import { useEffect, useMemo, useState } from "react";
+import { HOME_HEADING, HOME_SUBTITLE } from "@/shared/utils/constants";
 
 type PlantData = {
   plants: PlantDataType[];
   onLikeClick: () => void;
 };
 
+type CarouselPlant = {
+  currentId: number;
+  firstPlantImage: {
+    src: string;
+    alt: string;
+  };
+  secondPlantImage: {
+    src: string;
+    alt: string;
+  };
+};
+
 export default function Home({
   plants,
 }: // onLikeClick
 PlantData) {
+  const defaultState: CarouselPlant = useMemo(() => {
+    return {
+      currentId: 0,
+      firstPlantImage: {
+        src: plants[0]?.images[0]?.src,
+        alt: plants[0]?.images[0]?.alt,
+      },
+      secondPlantImage: {
+        src: plants[1]?.images[0]?.src,
+        alt: plants[2]?.images[0]?.alt,
+      },
+    };
+  }, [plants]);
+
+  const [currentState, setCurrentState] = useState<CarouselPlant>(defaultState);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentState.currentId >= plants.length - 2) {
+        setCurrentState(defaultState);
+      } else {
+        const prevId = currentState.currentId;
+        setCurrentState({
+          currentId: prevId + 2,
+          firstPlantImage: {
+            src: plants[prevId + 2]?.images[0]?.src,
+            alt: plants[prevId + 2]?.images[0]?.alt,
+          },
+          secondPlantImage: {
+            src: plants[prevId + 3]?.images[0]?.src,
+            alt: plants[prevId + 3]?.images[0]?.alt,
+          },
+        });
+      }
+    }, 4980);
+
+    return () => clearInterval(intervalId);
+  }, [currentState.currentId, defaultState, plants]);
+
   return (
     <>
       <StylesWrapper className="home-layout">
-        <Image
-          src={plants[0]?.images[0]?.src}
-          blurDataURL={imagePlaceholder}
-          alt={plants[0]?.images[0]?.alt}
+        <ImageCard
+          src={currentState.firstPlantImage.src}
+          alt={currentState.firstPlantImage.alt}
           width={227}
           height={600}
-          placeholder="blur"
+          carousel
           className="image-component-1"
-          style={{
-            objectFit: "cover",
-          }}
         />
-        <Image
-          src={plants[1]?.images[0]?.src}
-          blurDataURL={imagePlaceholder}
-          alt={plants[1]?.images[0]?.alt}
+        <ImageCard
+          src={currentState.secondPlantImage.src}
+          alt={currentState.secondPlantImage.alt}
           width={400}
           height={600}
-          placeholder="blur"
+          carousel
           className="image-component-2"
-          style={{
-            objectFit: "cover",
-          }}
         />
         <div className="text-wrapper">
-          <h1 className="homepage-heading">
-            Find the perfect plant for your place.
-          </h1>
-          <p className="homepage-subtitle">
-            Find your perfect flora companion! Search by color, water, level of
-            care family and much more. You can also learn about cool facts
-            information related to each plant.
-          </p>
+          <h1 className="homepage-heading">{HOME_HEADING}</h1>
+          <p className="homepage-subtitle">{HOME_SUBTITLE}</p>
           <button className="search-button">Search</button>
         </div>
       </StylesWrapper>
@@ -57,7 +97,7 @@ PlantData) {
 }
 
 export async function getServerSideProps() {
-  const data = await getPlants(0);
+  const data = await getPlants(0, "", 10);
 
   return {
     props: {
