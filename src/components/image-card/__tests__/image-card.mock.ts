9f379ff29414d712a1adccf60899aef869ef4ab1
anindyapramudita <anindyapramudita2111@gmail.com>
2023-09-01 12:29:35 +0200

    feat(image-card mock): expose mock for image card test

---
+++ src/components/image-card/__tests__/image-card.mock.ts
@@ -0,0 +1,13 @@
+export const ImageCardMock = {
+  src: "https://cdn.sanity.io/images/rb0oc6we/production/92c88bcfb234c49b3195e7645eaa45d552722fed-4000x6000.jpg",
+  alt: "plant-image",
+  width: 600,
+  height: 800,
+};
+
+export const EmptyImageMock = {
+  src: "",
+  alt: "",
+  width: 600,
+  height: 800,
+};
