768155daa6768fcdf0bbdc1a309402490f7cf0bc
anindyapramudita <anindyapramudita2111@gmail.com>
2023-09-01 12:30:23 +0200

    refactor(category-tab): simplify the component

--- src/components/category-tab/category-tab.tsx
+++ src/components/category-tab/category-tab.tsx
@@ -47,47 +47,52 @@ export const CategoryTab: FC<ICategoryTab> = ({ onClick }) => {
 
   return (
     <StylesWrapper
-      clicked={currentState.buttonClicked}
-      buttonStart={currentState.buttonStart}
       buttonEnd={currentState.buttonEnd}
+      data-testid="category-button-wrapper"
     >
       <button
         className="category-button button-1"
         onClick={() => handleClick("description", 1)}
+        data-testid="category-button"
       >
         <InfoIcon />
       </button>
       <button
         className="category-button button-2"
         onClick={() => handleClick("water", 2)}
+        data-testid="category-button"
       >
         <WaterIcon />
       </button>
       <button
         className="category-button button-3"
         onClick={() => handleClick("care", 3)}
+        data-testid="category-button"
       >
         <CareIcon />
       </button>
       <button
         className="category-button button-4"
         onClick={() => handleClick("seasons", 4)}
+        data-testid="category-button"
       >
         <SeasonIcon />
       </button>
       <button
         className="category-button button-5"
         onClick={() => handleClick("size", 5)}
+        data-testid="category-button"
       >
         <SizeIcon />
       </button>
       <button
         className="category-button button-6"
         onClick={() => handleClick("description", 6)}
+        data-testid="category-button"
       >
         <SunIcon />
       </button>
-      <div className="outline-wrapper">
+      <div className="outline-wrapper" data-testid="outline-wrapper">
         <div className="button-outline" />
         <div className="arrow-up" />
       </div>
