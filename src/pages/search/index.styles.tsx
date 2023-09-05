e164ff857d0614de77e508ee2805e59a35bb1318
anindyapramudita <anindyapramudita2111@gmail.com>
2023-09-05 11:30:23 +0200

    style(search page): update new style

--- src/pages/search/index.styles.tsx
+++ src/pages/search/index.styles.tsx
@@ -3,18 +3,31 @@ import styled from "styled-components";
 
 export const StylesWrapper = styled.div`
   --header-height: var(--header-height-mobile);
+  --page-padding: 32px;
 
   height: calc(100vh - var(--header-height));
   overflow-y: scroll;
-  padding: 32px 0;
+  padding: var(--page-padding);
   -ms-overflow-style: none;
   scrollbar-width: none;
+  tex-align: center;
 
   &::-webkit-scrollbar {
     display: none;
   }
 
   @media (min-width: ${gridBreakpoint.md}) {
+    --page-padding: 32px 64px;
+    --header-height: var(--header-height-desktop);
+  }
+
+  @media (min-width: ${gridBreakpoint.lg}) {
+    --page-padding: 32px 54px;
+    --header-height: var(--header-height-desktop);
+  }
+
+  @media (min-width: ${gridBreakpoint.xl}) {
+    --page-padding: 32px 64px;
     --header-height: var(--header-height-desktop);
   }
 `;
