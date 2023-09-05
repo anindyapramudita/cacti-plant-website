3ac9c0b4ddb9f14112d9de6433b75b3b34330bf1
anindyapramudita <anindyapramudita2111@gmail.com>
2023-09-05 11:29:34 +0200

    style(small-card-layout):  update new style

--- src/components/layouts/small-card/small-card-layout.styles.tsx
+++ src/components/layouts/small-card/small-card-layout.styles.tsx
@@ -4,24 +4,25 @@ import styled from "styled-components";
 export const StylesWrapper = styled.div<{}>`
   --gap-width: 1rem;
   --grid-column: 1fr;
-  --gap-per-row: 0;
   --card-per-row: 1;
   --padding-size: 2rem;
-  --gap-size: 2rem;
+  --gap-size: 32px;
   --padding-size: 0 2rem;
   --min-height: 110vh;
   --align-items: start;
 
-  display: grid;
-  grid-template-columns: repeat(var(--card-per-row), var(--grid-column));
   gap: var(--gap-size);
-  padding: var(--padding-size);
-  min-height: var(--min-height);
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
   align-items: var(--align-items);
-  width: 100%;
+  max-width: calc(300px * 5 + 32px * 4);
+  margin: auto;
 
   @media (min-width: ${gridBreakpoint.lg}) {
-    --gap-per-row: 2;
-    --card-per-row: 5;
+  }
+
+  @media (min-width: ${gridBreakpoint.lg}) {
   }
 `;
