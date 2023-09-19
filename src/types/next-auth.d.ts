744b11ee95dbfb32ee66d3daf415a9257890d739
anindyapramudita <anindyapramudita2111@gmail.com>
2023-09-19 14:22:06 +0200

    chore(next-auth.d.ts): move file to types

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,32 @@
+// import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      _id: string;
+      name: string;
+      email: string;
+      role: string;
+      image: any;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/client" {
+  export * from "next-auth/client";
+
+  export interface Session {
+    user: {
+      _id: string;
+      name: string;
+      email: string;
+      role: string;
+      image: any;
+    };
+  }
+}
