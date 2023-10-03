d6a107e4563373fd7f3368593b7748946e95d672
anindyapramudita <anindyapramudita2111@gmail.com>
2023-10-03 11:30:54 +0200

    refactor(like-plant): update body using _id

--- src/shared/utils/like-plant.ts
+++ src/shared/utils/like-plant.ts
@@ -1,7 +1,7 @@
-export const likePlant = async (email: string, id: string) => {
+export const likePlant = async (userId: string, id: string) => {
   const response = await fetch("/api/like-plant", {
     method: "POST",
-    body: JSON.stringify({ email, id }),
+    body: JSON.stringify({ userId, id }),
     headers: {
       "Content-Type": "application/json",
     },
