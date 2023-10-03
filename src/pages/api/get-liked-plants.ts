deaa6fcdc0056899af8a05e55151f012f2e68384
anindyapramudita <anindyapramudita2111@gmail.com>
2023-10-03 11:18:31 +0200

    feat(get-liked-plants): expose new api to get user liked plants

--- src/pages/api/get-liked-plants.ts
+++ src/pages/api/get-liked-plants.ts
@@ -1,12 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "@/db/utils/dbConnect";
 import User from "@/db/models/user";
-import { IMAGE_LIKED_SUCCESS, SOMETHING_WRONG } from "@/shared/utils/constants";
+import {
+  LIKED_FETCH_FAILED,
+  LIKED_FETCH_SUCCESSFUL,
+} from "@/shared/utils/constants";
+import { client } from "@/sanity/get-client";
+import { groq } from "next-sanity";
 
 type Data = {
   success: boolean;
   message: string;
   likedPlants: string[];
+  likedPlantsId: string[];
+};
+
+const getIds = (id: string[]) => {
+  const idQuery: string[] = [];
+  id.forEach((plantId: string) => {
+    idQuery.push(`_id == "${plantId}"`);
+  });
+  return idQuery.join(" || ");
 };
 
 export default async function handler(
@@ -15,21 +29,53 @@ export default async function handler(
 ) {
   dbConnect();
 
-  const email = request.body.email;
-  const updateResponse = await User.findOne({ email }).select("likedPlants");
+  const _id = request.body.userId;
+  const updateResponse = await User.findOne({ _id }).select("likedPlants");
 
   if (updateResponse) {
-    response.status(201).json({
-      likedPlants: updateResponse,
-      success: true,
-      message: IMAGE_LIKED_SUCCESS,
+    if (updateResponse.likedPlants.length > 0) {
+      const idQuery = getIds(updateResponse.likedPlants);
+
+      const likedPlants =
+        await client.fetch(groq`*[_type == "plant" && (${idQuery})]{
+        _id,
+        _createdAt,
+        name,
+        images[]->{
+          alt,
+          "src": src.asset->url
+        },
+        description,
+        water,
+        care,
+        seasons,
+        size,
+        color,
+        "total": count(*[_type == "plant"])
+      }`);
+
+      response.status(201).json({
+        likedPlants,
+        likedPlantsId: updateResponse.likedPlants,
+        success: true,
+        message: LIKED_FETCH_SUCCESSFUL,
+      });
+    } else {
+      response.status(201).json({
+        likedPlants: [],
+        likedPlantsId: updateResponse.likedPlants,
+        success: true,
+        message: LIKED_FETCH_SUCCESSFUL,
+      });
+    }
+  } else {
+    response.status(404).json({
+      likedPlants: [],
+      likedPlantsId: [],
+      success: false,
+      message: LIKED_FETCH_FAILED,
     });
   }
 
-  response.status(404).json({
-    likedPlants: [],
-    success: false,
-    message: SOMETHING_WRONG,
-  });
   // }
 }
