de8230b2d7a8c62786606d57ad661aa7d11ab887
anindyapramudita <anindyapramudita2111@gmail.com>
2023-10-03 11:16:54 +0200

    feat(toast): expose new component

---
+++ src/components/toast/toast.tsx
@@ -0,0 +1,51 @@
+import { FC } from "react";
+import { StylesWrapper } from "./toast.styles";
+import { IToastProps, ToastType } from "./toast.interface";
+import classNames from "classnames";
+import { AiFillCheckCircle, AiFillExclamationCircle } from "react-icons/ai";
+import { BsFillExclamationTriangleFill } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
+import { IconButton } from "../icon-button";
+
+export const Toast: FC<IToastProps> = ({
+  children,
+  type,
+  position,
+  onClose,
+}) => {
+  const positions = position.split("-");
+
+  const getICon = (type: ToastType) => {
+    if (type === "success")
+      return <AiFillCheckCircle className="message-icon" />;
+    if (type === "error")
+      return <AiFillExclamationCircle className="message-icon" />;
+    return <BsFillExclamationTriangleFill className="message-icon" />;
+  };
+
+  if (!children) {
+    return null;
+  }
+
+  return (
+    <StylesWrapper
+      className={classNames(
+        { "toast-top": positions[0] === "top" },
+        { "toast-bottom": positions[0] === "bottom" },
+        { "toast-left": positions[1] === "left" },
+        { "toast-right": positions[1] === "right" },
+        { "toast-middle": positions[1] === "middle" },
+        `toast-${type}`
+      )}
+    >
+      <div className="toast-message">
+        {getICon(type)}
+        {children}
+      </div>
+      <IconButton
+        icon={<IoMdClose className="close-icon" />}
+        onClick={onClose}
+      />
+    </StylesWrapper>
+  );
+};
