cf7d6f633434f99523565b6b3d0db42f8fdb03c9
anindyapramudita <anindyapramudita2111@gmail.com>
2023-10-10 17:37:52 +0200

    refactor(navigation): update new data structure for navigation

--- src/shared/navigation.tsx
+++ src/shared/navigation.tsx
@@ -1,66 +1,62 @@
-import { ReactElement } from "react";
-import {
-  AiOutlinePhone,
-  AiOutlineSearch,
-  AiOutlineUser,
-  AiOutlineUserAdd,
-} from "react-icons/ai";
-import { BiShuffle, BiLogOut } from "react-icons/bi";
-import { BsCollectionPlay } from "react-icons/bs";
-
 export type Navigation = {
   path: string;
   name: string;
-  logo: ReactElement;
   roles: string[];
 };
 export const navigation: Navigation[] = [
   {
-    path: "/contact-us",
-    name: "Contact Us",
-    logo: <AiOutlinePhone size={30} />,
-    roles: ["admin", "user", "all"],
-  },
-  {
-    path: "/shuffle",
-    name: "Shuffle",
-    logo: <BiShuffle size={30} />,
-    roles: ["admin", "user", "all"],
-  },
-  {
-    path: "/search",
-    name: "Search",
-    logo: <AiOutlineSearch size={30} />,
-    roles: ["admin", "user", "all"],
-  },
-  {
     path: "#",
-    name: "Login",
-    logo: <AiOutlineUser size={30} />,
+    name: "Sign In",
     roles: ["admin", "all"],
   },
   {
     path: "/register",
     name: "Sign Up",
-    logo: <AiOutlineUserAdd size={30} />,
     roles: ["admin", "all"],
-  },
-  {
-    path: "/collection",
-    name: "Collection",
-    logo: <BsCollectionPlay size={30} />,
-    roles: ["user"],
   },
   {
     path: "/profile",
     name: "Profile",
-    logo: <AiOutlineUser size={30} />,
+    roles: ["user"],
+  },
+  {
+    path: "#",
+    name: "Divider",
+    roles: ["user", "admin", "all"],
+  },
+  {
+    path: "/shuffle",
+    name: "Shuffle",
+    roles: ["admin", "user", "all"],
+  },
+  {
+    path: "/search",
+    name: "Plants",
+    roles: ["admin", "user", "all"],
+  },
+  {
+    path: "#",
+    name: "Divider",
+    roles: ["user", "admin", "all"],
+  },
+  {
+    path: "/contact-us",
+    name: "Contact Us",
+    roles: ["admin", "user", "all"],
+  },
+  {
+    path: "/about-us",
+    name: "About Us",
+    roles: ["admin", "user", "all"],
+  },
+  {
+    path: "#",
+    name: "Divider",
     roles: ["user"],
   },
   {
     path: "#",
     name: "Log out",
-    logo: <BiLogOut size={30} />,
     roles: ["user"],
   },
 ];
