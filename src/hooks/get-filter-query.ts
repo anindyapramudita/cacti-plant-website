408bea2321a3e099e260946df1ab18cab8d6e4b3
anindyapramudita <anindyapramudita2111@gmail.com>
2023-10-10 17:41:31 +0200

    refactor(get-filter-query): update get filter query with new data structure

--- src/hooks/get-filter-query.ts
+++ src/hooks/get-filter-query.ts
@@ -1,34 +1,54 @@
-import { Filter } from "@/hooks/use-plant-filter";
+import { Filter } from "./use-plant-filter";
 
 export const handleUpdateFilter = (filterContext: Filter) => {
   let query: string[] = [];
 
   if (filterContext.search) {
-    query.push(`name match "*${filterContext.search}*"`);
+    if (typeof filterContext.search === "string") {
+      query.push(`name match "*${filterContext.search}*"`);
+    } else if (filterContext.search.length > 0) {
+      query.push(`name match "*${filterContext.search[0]}*"`);
+    }
   }
 
   if (filterContext.filter.water) {
-    query.push(`water.level == ${filterContext.filter.water}`);
+    if (typeof filterContext.filter.water === "string") {
+      query.push(`water.level == ${filterContext.filter.water}`);
+    } else if (filterContext.filter.water.length > 0) {
+      query.push(`water.level == ${filterContext.filter.water}`);
+    }
   }
 
   if (filterContext.filter.care) {
-    query.push(`care.level == ${filterContext.filter.care}`);
+    if (typeof filterContext.filter.care === "string") {
+      query.push(`care.level == ${filterContext.filter.care}`);
+    } else if (filterContext.filter.care.length > 0) {
+      query.push(`care.level == ${filterContext.filter.care[0]}`);
+    }
   }
 
-  if (filterContext.filter.size.length > 0) {
-    let temp: string[] = [];
-    filterContext.filter.size.forEach((careValue: number) => {
-      temp.push(`size.level == ${careValue}`);
-    });
-    query.push(`(${temp.join(" || ")})`);
+  if (filterContext.filter.size) {
+    if (typeof filterContext.filter.size === "string") {
+      query.push(`size.level == ${filterContext.filter.size}`);
+    } else if (filterContext.filter.size.length > 0) {
+      let temp: string[] = [];
+      filterContext.filter.size.forEach((size: number | string) => {
+        temp.push(`size.level == ${size}`);
+      });
+      query.push(`(${temp.join(" || ")})`);
+    }
   }
 
-  if (filterContext.filter.seasons.length > 0) {
-    let temp: string[] = [];
-    filterContext.filter.seasons.forEach((seasonValue: number | string) => {
-      temp.push(`seasons.${seasonValue} == true`);
-    });
-    query.push(`(${temp.join(" || ")})`);
+  if (filterContext.filter.seasons) {
+    if (typeof filterContext.filter.seasons === "string") {
+      query.push(`seasons.${filterContext.filter.seasons} == true`);
+    } else if (filterContext.filter.seasons.length > 0) {
+      let temp: string[] = [];
+      filterContext.filter.seasons.forEach((seasonValue: number | string) => {
+        temp.push(`seasons.${seasonValue} == true`);
+      });
+      query.push(`(${temp.join(" || ")})`);
+    }
   }
 
   const finalQuery = query.length > 0 ? `&& ${query.join(" && ")}` : null;
